c0a0f5655c360d202023387e3f4d76c28c51924f
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-09-13 10:35:18 +0200

    ADD nom appli sur page d'accueil

--- CrazyDev/resources/views/welcome.blade.php
+++ CrazyDev/resources/views/welcome.blade.php
@@ -54,9 +54,13 @@
         <div>
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                    @auth
-                        <h1 style="text-align: center; margin-bottom: 5rem; font-size: 125%;">Bienvenue {{ Auth::user()->first_name }}.</h1>
-                    @endauth
+                    @if (Route::has('login'))
+                        @auth
+                            <h1 style="text-align: center; margin-bottom: 5rem; font-size: 125%;">Bienvenue sur OrbitHub, {{ Auth::user()->first_name }}.</h1>
+                    @else
+                            <h1 style="text-align: center; margin-bottom: 5rem; font-size: 125%;">Bienvenue sur OrbitHub !</h1>
+                        @endauth
+                    @endif
                     <header class="py-10 flex justify-center">
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 navigation-login">
